ed314c4f04c3f84b5c78b5192a61f5ecb9d76235
demas59 <demasay@gmail.com>
2020-09-26 18:32:38 +0200

    refacto axios

--- front/src/service/resources/resources.js
+++ front/src/service/resources/resources.js
@@ -1,26 +1,19 @@
-import Vue from 'vue';
 
+import axios from 'axios'; 
 export default class Resources {
-
-
+    axios = require('axios');
     getPrintedWidgets(params) {
         const url = `${process.env.VUE_APP_BACK_URL}`
-        let config = {
-            params: params
-        }
-        return Vue.http.get(url, config)
+        return axios.get(url, {params:params});
     }
 
     getMeteo(params) {
         const url = `https://api.openweathermap.org/data/2.5/onecall?lat=50.55&lon=3.03&exclude=current,minutely,hourly,alerts&appid=ee95de4f37a7e21b3714e529ea39a2fb&units=metric`;
-        let config = {
-            params: params
-        }
-        return Vue.http.get(url, config)
+        return axios.get(url, {params:params});
     }
 
     getCity(params) {
         const url = `https://geocode.xyz/${params.lat},${params.long}?geoit=json`;
-        return Vue.http.get(url);
+        return axios.get(url);
     }
 }
