66ac29270035b83dc7fd3ec8ed98bc1be1d48d06
Marchand <1ttoommyy@gmail.com>
2020-09-29 11:17:13 +0200

    config parametrage

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -18,6 +18,12 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/parametrage",
+    name: "Parametrage",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Parametrage.vue")
   }
 ];
 
---
+++ front/src/views/Parametrage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "parametrage",
+  components: {
+      
+  },
+}
+</script>
+
+<style scoped>
+div{
+    color:white
+}
+</style>
