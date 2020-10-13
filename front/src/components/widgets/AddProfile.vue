d875cd91120823429a2e995da56fe968b5c2df24
YoOpii <julien.dupuis04@gmail.com>
2020-10-13 11:19:18 +0200

    AddProfile.vue

--- front/src/components/widgets/AddProfile.vue
+++ front/src/components/widgets/AddProfile.vue
@@ -1,18 +1,29 @@
 <template>
 <div class="addProfile" align="center" justify="center">
-
-    <h1>Ajouter un profil</h1>
+    <v-container>
+        <v-sheet
+            rounded
+            color="transparent"
+            elevation="4"
+            style="margin-top: 60px"
+            height="500"
+            width="350"
+        >
     
-    <v-text-field
-        class = "profileName"
-        v-model="name"
-        label="Nom de profil"
-        dark
-    ></v-text-field>
-    <div class="buttons"> 
-        <v-btn elevation="2" class="button" light>Ajouter</v-btn>
-        <v-btn elevation="2" class="button" light>Annuler</v-btn>
-    </div>
+        <h1>Ajouter un profil</h1>
+    
+        <v-text-field
+            class = "profileName"
+            v-model="name"
+            label="Nom de profil"
+            dark
+        ></v-text-field>
+        <div class="buttons"> 
+            <v-btn elevation="2" class="button" light>Ajouter</v-btn>
+            <v-btn elevation="2" class="button" light>Annuler</v-btn>
+        </div>
+        </v-sheet>
+    </v-container> 
 </div>
 </template>
 
@@ -21,20 +32,24 @@
 </script>
 
 <style scoped>
+
 .addProfile {
     background-color: #3c3e41;
     height: 100%;
     color: white;
+    
 }
 
+
 h1 {
+    color: white;
     font-size: 30px;
-    padding-top: 5%;
+    padding-top: 20%;
     font-weight: bold;
 }
 
 .profileName {
-    margin-top: 10%;
+    margin-top: 30%;
     width: 160px;
 
 }
@@ -44,7 +59,7 @@ h1 {
 }
 
 .buttons{
-    margin-top: 5%
+    margin-top: 20%
 }
 
 
