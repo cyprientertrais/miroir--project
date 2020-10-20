794aaf36af431dc0d5b0153733b0cfaf98053bd4
alex <alexandreazzaro@gmail.com>
2020-10-20 15:46:39 +0200

    V-list rounded

--- front/src/components/WifiChoice.vue
+++ front/src/components/WifiChoice.vue
@@ -1,27 +1,50 @@
 <template>
+<v-container>
   <div align="center" justify="center" class="wifi">
     <div class="titre">IMirror</div>
     <div class="Wifi">
-    {{/*  <v-text-field
-        v-model="password"
-        v-on:keyup="inputKeyPressed"
-        label="Admin password"
-        type="password"
-        class="inputPassword"
-        dark
-        :error="isPasswordWrong"
-        :error-messages="wrongPasswordMessage"
-      ></v-text-field>*/}}
-    <select v-model="selected" multiple>
-      <option>A</option>
-      <option>B</option>
-      <option>C</option>
-    </select>
-  <br>
-{{/*<span>Sélectionné(s) : {{ selected }}</span>*/}}
+    <v-card
+    class="mx-auto"
+    max-width=75%
+    max-height=100%
+    tile
+  >
+    <v-list rounded>
+      <v-subheader>REPORTS</v-subheader>
+      <v-list-item-group
+        v-model="item"
+        color="primary"
+      >
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-item-icon>
+            <v-icon v-text="item.icon"></v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.text"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-card>
       <v-btn v-on:click="WifiTentative" elevation="2" class="selectWifi" dark
         >selectWifi</v-btn
       >
     </div>
   </div>
+  </v-container>
 </template>
+
+<script>
+export default {
+  methods :{
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
