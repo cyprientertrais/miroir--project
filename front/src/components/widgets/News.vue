9885ad0329ed5e590187932890a2a5dfcb1dbdf5
Cypnose (Cypnose) <tertraiscyprien64@gmail.com>
2020-10-28 18:07:00 +0100

    (add): vcard news

--- front/src/components/widgets/News.vue
+++ front/src/components/widgets/News.vue
@@ -10,23 +10,27 @@
         fill-height fluid
  
       >
-        <v-row
-        justify="center"
-        align="center"
-        >
-          <div class="content">
-           <span  class="news">{{el.title}}</span>
-           <img :src="el.enclosure.url" width="100%"/>
-          </div>
-          <div class="date">{{moment(el.isoDate).fromNow()}}</div>
-        </v-row>
+         <v-card class="mx-auto">
+      <v-img
+        :src="el.enclosure.url"
+       
+      ></v-img>
+  
+      <v-card-title>
+        {{el.title}}
+      </v-card-title>
+  
+      <v-card-subtitle>
+       {{moment(el.isoDate).fromNow()}}
+      </v-card-subtitle>
+         </v-card>
+
       </v-sheet>
     </v-carousel-item>
   </v-carousel>
 </template>
 
 <script>
-// import Day from "./Day";
 import Resources from "@/service/resources/resources";
 var moment = require("moment");
 const ResourcesService = new Resources();
@@ -46,37 +50,14 @@ export default {
   data() {
     return {
     model:0,
-     news:null
+    news:null
     };
   },
-  
   methods: {
     moment(d){
       return moment(d);
     }
    
   },
-  watch: {
-  }
 };
 </script>
-
-<style>
-span {
-  color: white;
-}
-.content{
-width: 100%;
-margin-left: 15px;
-margin-right: 10px;
-text-align: center;
-}
-.news{
-  font-size: 20px;
-  
-}
-.date{
-  text-align: right;
-  justify-content: right;
-}
-</style>
