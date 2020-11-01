86b0b7649af0db0f2b45c2cda1a92d648924ae38
Alexandre <alexandre.ansel.98@gmail.com>
2020-11-01 11:43:03 +0100

    lateral sensor

--- python/InterruptMiroir.py
+++ python/InterruptMiroir.py
@@ -3,23 +3,36 @@
 import RPi.GPIO as GPIO  
 from time import sleep
 
+# Désactive les warnings
+GPIO.setwarnings(False)
+
+# Mode BCM de la Pi
 GPIO.setmode(GPIO.BCM)  
 
-pinCapteur = 26
-pinEcran = 19
+# Définition des pins
+pinCapteur = 26 # Signal du capteur
+pinEcran = 19   # Signal de l'écran
 
+# Définition des GPIO
+# On envoie le signal à l'écran et son état initial est haut
 GPIO.setup(pinEcran, GPIO.OUT, initial=GPIO.HIGH)
+# On reçoit le signal du capteur et on active la résistance pull-up
 GPIO.setup(pinCapteur, GPIO.IN, pull_up_down=GPIO.PUD_UP) 
   
+# Boucle
 while True :
-    GPIO.wait_for_edge(pinCapteur, GPIO.RISING)
-    # On appui sur le bouton
-    GPIO.output(pinEcran, GPIO.DOWN)
-    sleep(0.5)
-    # On relache le bouton
-    GPIO.output(pinEcran, GPIO.UP)
-    # On attend pour être certain que l'utilisateur ne lance pas plusieurs
-    # instances lorsqu'il passe pas sa main plusieurs fois.
-    sleep(2)
+    try:
+        # On attent un front montant
+        GPIO.wait_for_edge(pinCapteur, GPIO.RISING)
+        # On appui sur le bouton
+        GPIO.output(pinEcran, GPIO.LOW)
+        sleep(0.5)
+        # On relache le bouton
+        GPIO.output(pinEcran, GPIO.HIGH)
+        # On attend pour être certain que l'utilisateur ne lance pas plusieurs
+        # instances lorsqu'il passe pas sa main plusieurs fois.
+        sleep(2)
 
-# GPIO.add_event_detect(pinCapteur, GPIO.RISING, callback=my_callback)
+    # En cas d'arrêt par Ctrl+C, on réinitialise les pins
+    except KeyboardInterrupt:
+        GPIO.cleanup()
